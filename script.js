e0ef52be5c65254f513ba24916471b73ce9505dc
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2023-07-19 16:49:17 +0530

    Change * to x in operate

--- script.js
+++ script.js
@@ -6,10 +6,11 @@ function subtract(a, b) {
   return a - b;
 }
 
-function division(a, b) {
+function divide(a, b) {
   if (b !== 0) return a / b;
   else {
     alert("can't divide by 0");
+    return a/b;
   }
 }
 
@@ -25,7 +26,7 @@ function operate(firstNum,secondNum,operator){
     switch(operator){
         case "+": return add(firstNum,secondNum);
         case "-": return subtract(firstNum,secondNum);
-        case "*": return multiply(firstNum,secondNum);
+        case "x": return multiply(firstNum,secondNum);
         case "/": return divide(firstNum,secondNum);
     }
 }
